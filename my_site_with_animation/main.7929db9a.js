4541dc8ed810e16182418a9b203cb2a1d09df0ef
vanzo16 <vanzo16@ya.ru>
2018-08-20 11:15:36 +0300

    New site
    Add files

---
+++ my_site_with_animation/main.7929db9a.js
@@ -0,0 +1,508 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"bideo.js":[function(require,module,exports) {
+var global = arguments[3];
+/**
+ * Full Background Video
+ *
+ * More info on Audio/Video Media Events/Attributes/Methods
+ * - https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
+ * - http://www.w3schools.com/tags/ref_av_dom.asp
+ */
+
+(function (global) {
+
+  // Define Bideo constructor on the global object
+  global.Bideo = function () {
+
+    // Plugin options
+    this.opt = null;
+    // The Video element
+    this.videoEl = null;
+
+    // Approximate Loading Rate
+    //
+    // The value will be a number like 0.8
+    // which means to load 4 seconds of the video
+    // it takes 5 seconds. If the number is super low
+    // like 0.2 (regular 3g connections) then you can
+    // decide whether to play the video or not.
+    // This behaviour will be controller with
+    // the `acceptableLoadingRate` option.
+    this.approxLoadingRate = null;
+
+    // Methods to which `this` will be bound
+    this._resize = null;
+    this._progress = null;
+
+    // Time at which video is initialized
+    this.startTime = null;
+
+    this.onLoadCalled = false;
+
+    // Initialize and setup the video in DOM`
+    this.init = function (opt) {
+      // If not set then set to an empty object
+      this.opt = opt = opt || {};
+
+      var self = this;
+
+      self._resize = self.resize.bind(this);
+
+      // Video element
+      self.videoEl = opt.videoEl;
+
+      // Meta data event
+      self.videoEl.addEventListener('loadedmetadata', self._resize, false);
+
+      // Fired when enough has been buffered to begin the video
+      // self.videoEl.readyState === 4 (HAVE_ENOUGH_DATA)
+      self.videoEl.addEventListener('canplay', function () {
+        // Play the video when enough has been buffered
+        if (!self.opt.isMobile) {
+          self.opt.onLoad && self.opt.onLoad();
+          if (self.opt.autoplay !== false) self.videoEl.play();
+        }
+      });
+
+      // If resizing is required (resize video as window/container resizes)
+      if (self.opt.resize) {
+        global.addEventListener('resize', self._resize, false);
+      }
+
+      // Start time of video initialization
+      this.startTime = new Date().getTime();
+
+      // Create `source` for video
+      this.opt.src.forEach(function (srcOb, i, arr) {
+        var key,
+            val,
+            source = document.createElement('source');
+
+        // Set all the attribute key=val supplied in `src` option
+        for (key in srcOb) {
+          if (srcOb.hasOwnProperty(key)) {
+            val = srcOb[key];
+
+            source.setAttribute(key, val);
+          }
+        }
+
+        self.videoEl.appendChild(source);
+      });
+
+      if (self.opt.isMobile) {
+        if (self.opt.playButton) {
+          self.opt.videoEl.addEventListener('timeupdate', function () {
+            if (!self.onLoadCalled) {
+              self.opt.onLoad && self.opt.onLoad();
+              self.onLoadCalled = true;
+            }
+          });
+
+          self.opt.playButton.addEventListener('click', function () {
+            self.opt.pauseButton.style.display = 'inline-block';
+            this.style.display = 'none';
+
+            self.videoEl.play();
+          }, false);
+
+          self.opt.pauseButton.addEventListener('click', function () {
+            this.style.display = 'none';
+            self.opt.playButton.style.display = 'inline-block';
+
+            self.videoEl.pause();
+          }, false);
+        }
+      }
+
+      return;
+    };
+
+    // Called once video metadata is available
+    //
+    // Also called when window/container is resized
+    this.resize = function () {
+      // IE/Edge still don't support object-fit: cover
+      if ('object-fit' in document.body.style) return;
+
+      // Video's intrinsic dimensions
+      var w = this.videoEl.videoWidth,
+          h = this.videoEl.videoHeight;
+
+      // Intrinsic ratio
+      // Will be more than 1 if W > H and less if H > W
+      var videoRatio = (w / h).toFixed(2);
+
+      // Get the container DOM element and its styles
+      //
+      // Also calculate the min dimensions required (this will be
+      // the container dimentions)
+      var container = this.opt.container,
+          containerStyles = global.getComputedStyle(container),
+          minW = parseInt(containerStyles.getPropertyValue('width')),
+          minH = parseInt(containerStyles.getPropertyValue('height'));
+
+      // If !border-box then add paddings to width and height
+      if (containerStyles.getPropertyValue('box-sizing') !== 'border-box') {
+        var paddingTop = containerStyles.getPropertyValue('padding-top'),
+            paddingBottom = containerStyles.getPropertyValue('padding-bottom'),
+            paddingLeft = containerStyles.getPropertyValue('padding-left'),
+            paddingRight = containerStyles.getPropertyValue('padding-right');
+
+        paddingTop = parseInt(paddingTop);
+        paddingBottom = parseInt(paddingBottom);
+        paddingLeft = parseInt(paddingLeft);
+        paddingRight = parseInt(paddingRight);
+
+        minW += paddingLeft + paddingRight;
+        minH += paddingTop + paddingBottom;
+      }
+
+      // What's the min:intrinsic dimensions
+      //
+      // The idea is to get which of the container dimension
+      // has a higher value when compared with the equivalents
+      // of the video. Imagine a 1200x700 container and
+      // 1000x500 video. Then in order to find the right balance
+      // and do minimum scaling, we have to find the dimension
+      // with higher ratio.
+      //
+      // Ex: 1200/1000 = 1.2 and 700/500 = 1.4 - So it is best to
+      // scale 500 to 700 and then calculate what should be the
+      // right width. If we scale 1000 to 1200 then the height
+      // will become 600 proportionately.
+      var widthRatio = minW / w;
+      var heightRatio = minH / h;
+
+      // Whichever ratio is more, the scaling
+      // has to be done over that dimension
+      if (widthRatio > heightRatio) {
+        var new_width = minW;
+        var new_height = Math.ceil(new_width / videoRatio);
+      } else {
+        var new_height = minH;
+        var new_width = Math.ceil(new_height * videoRatio);
+      }
+
+      this.videoEl.style.width = new_width + 'px';
+      this.videoEl.style.height = new_height + 'px';
+    };
+  };
+})(window);
+},{}],"main.js":[function(require,module,exports) {
+'use strict';
+
+require('./bideo');
+
+(function () {
+
+  var bv = new Bideo();
+  bv.init({
+    // Video element
+    videoEl: document.querySelector('#background_video'),
+
+    // Container element
+    container: document.querySelector('body'),
+
+    // Resize
+    resize: true,
+
+    // autoplay: false,
+
+    isMobile: window.matchMedia('(max-width: 768px)').matches,
+
+    playButton: document.querySelector('#play'),
+    pauseButton: document.querySelector('#pause'),
+
+    // Array of objects containing the src and type
+    // of different video formats to add
+    src: [{
+      src: './night.mp4',
+      type: 'video/mp4'
+    }, {
+      src: './night.webm',
+      type: 'video/webm;codecs="vp8, vorbis"'
+    }],
+
+    // What to do once video loads (initial frame)
+    onLoad: function onLoad() {
+      document.querySelector('#video_cover').style.display = 'none';
+    }
+  });
+})();
+},{"./bideo":"bideo.js"}],"C:\\Users\\vanzo\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '64847' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+
+      removeErrorOverlay();
+
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["C:\\Users\\vanzo\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js","main.js"], null)
+//# sourceMappingURL=/main.7929db9a.map
